486c427524dde46699f59c47c2cad17ebd13dce6
long.vu <vulonggiao0207@gmail.com>
2016-04-20 11:24:41 +1000

    Initial

---
+++ MVCFoodOrder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCFoodOrder.Startup))]
+namespace MVCFoodOrder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
